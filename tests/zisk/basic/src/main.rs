466cb04a9f65221e3dca76da380537d4df1f6713
han0110 <tinghan0110@gmail.com>
2025-08-27 11:39:50 +0000

    feat: implement returning of public values for `openvm`, and add `OutputHashedIo` to test it

--- tests/zisk/basic/src/main.rs
+++ tests/zisk/basic/src/main.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
 use core::array::from_fn;
-use test_utils::guest::{BasicStruct, BASIC_PROGRAM_BYTES_LENGTH};
+use test_utils::guest::{BasicProgramCore, BasicStruct};
 
 ziskos::entrypoint!(main);
 
@@ -19,14 +19,14 @@ fn main() {
     assert!(input.is_empty());
 
     // Check `bytes` length is as expected.
-    assert_eq!(bytes.len(), BASIC_PROGRAM_BYTES_LENGTH);
+    assert_eq!(bytes.len(), BasicProgramCore::BYTES_LENGTH);
 
-    // Do some computation on `basic_struct`.
-    let basic_struct_output = basic_struct.output();
+    // Do some computation on `bytes` and `basic_struct`.
+    let (rev_bytes, basic_struct_output) = BasicProgramCore::outputs((bytes, basic_struct));
 
-    // Write reversed `bytes` and `basic_struct_output`
+    // Write `rev_bytes` and `basic_struct_output`
     let public_values = core::iter::empty()
-        .chain(bytes.into_iter().rev())
+        .chain(rev_bytes)
         .chain(bincode::serialize(&basic_struct_output).unwrap())
         .collect::<Vec<_>>();
     public_values
