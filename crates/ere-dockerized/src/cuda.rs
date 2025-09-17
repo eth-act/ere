b608bf271122e2543d247bb3dea5cd180151137d
han0110 <tinghan0110@gmail.com>
2025-09-17 02:17:10 +0000

    fix: clippy

--- crates/ere-dockerized/src/cuda.rs
+++ crates/ere-dockerized/src/cuda.rs
@@ -1,5 +1,5 @@
 use std::{env, process::Command};
-use tracing::info;
+use tracing::{info, warn};
 
 /// Returns Cuda GPU compute capability, for example
 /// - RTX 50 series - returns `12.0`
@@ -33,12 +33,16 @@ pub fn cuda_compute_cap() -> Option<String> {
 ///
 /// Otherwise it returns `None`.
 pub fn cuda_arch() -> Option<String> {
-    if let Ok(cuda_arch) = env::var("CUDA_ARCH")
-        && cuda_arch.starts_with("sm_")
-        && cuda_arch[3..].parse::<usize>().is_ok()
-    {
-        info!("Using CUDA_ARCH {} from env variable", cuda_arch);
-        Some(cuda_arch)
+    if let Ok(cuda_arch) = env::var("CUDA_ARCH") {
+        if cuda_arch.starts_with("sm_") && cuda_arch[3..].parse::<usize>().is_ok() {
+            info!("Using CUDA_ARCH {cuda_arch} from env variable");
+            Some(cuda_arch)
+        } else {
+            warn!(
+                "Skipping CUDA_ARCH {cuda_arch} from env variable (expected to be in format `sm_XX`)"
+            );
+            None
+        }
     } else if let Some(cap) = cuda_compute_cap() {
         info!("Using CUDA compute capability {} detected", cap);
         Some(format!("sm_{}", cap.replace(".", "")))
