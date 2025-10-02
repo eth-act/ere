9bd082c2cb6f77959103f5a5c9e7ea8d294c2834
rodiazet <radek.zagorowicz@gmail.com>
2025-10-02 11:13:34 +0200

    support risc0 profiling in docker.

--- crates/ere-dockerized/src/lib.rs
+++ crates/ere-dockerized/src/lib.rs
@@ -390,9 +390,11 @@ impl zkVM for EreDockerizedzkVM {
         let prof_file_out_name = prof_file_out_path.file_name().unwrap();
         let prof_file_out_path_host = PathBuf::from(tempdir.path().join(prof_file_out_name));
 
+        println!("Copying pprof result file to {}", env::current_dir().unwrap().display());
         if fs::exists(&prof_file_out_path_host).unwrap() {
             fs::copy(prof_file_out_path_host, env::current_dir().unwrap().join(prof_file_out_name)).expect("Cannot copy pprof result file");
         }
+        else { println!("pprof result file does not exist"); }
 
         Ok((public_values, report))
     }
