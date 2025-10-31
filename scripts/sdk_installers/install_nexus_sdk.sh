f423424875cd249711adf0a938c5490435bd1219
han0110 <tinghan0110@gmail.com>
2025-10-31 14:00:18 +0000

    feat: upgrade `nexus` to `0.3.5`

--- scripts/sdk_installers/install_nexus_sdk.sh
+++ scripts/sdk_installers/install_nexus_sdk.sh
@@ -31,7 +31,7 @@ ensure_tool_installed "rustup" "for managing Rust toolchains"
 ensure_tool_installed "cargo" "as cargo-nexus is a cargo subcommand"
 
 NEXUS_TOOLCHAIN_VERSION="nightly-2025-04-06"
-NEXUS_CLI_VERSION_TAG="v0.3.4"
+NEXUS_CLI_VERSION_TAG="v0.3.5"
 
 # Install the Nexus CLI
 echo "Installing Nexus CLI from GitHub repository..."
