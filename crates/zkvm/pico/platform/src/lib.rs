41698b4578512d99e235d70016a5e93fff5b1b4d
han0110 <tinghan0110@gmail.com>
2025-11-03 07:59:52 +0000

    feat: add crate `ere-platform-pico`

---
+++ crates/zkvm/pico/platform/src/lib.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![no_std]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+pub use ere_platform_trait::Platform;
+pub use pico_sdk;
+
+pub struct PicoPlatform;
+
+impl Platform for PicoPlatform {
+    fn read_whole_input() -> Vec<u8> {
+        pico_sdk::io::read_vec()
+    }
+
+    fn write_whole_output(output: &[u8]) {
+        pico_sdk::io::commit_bytes(output);
+    }
+}
