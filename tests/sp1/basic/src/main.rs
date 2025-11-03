b19cb4a7dbf8311eee38a39949690ec3d152e075
han0110 <tinghan0110@gmail.com>
2025-11-03 11:45:34 +0000

    feat: add crate `ere-platform-sp1`

--- tests/sp1/basic/src/main.rs
+++ tests/sp1/basic/src/main.rs
@@ -1,23 +1,9 @@
 #![no_main]
 
-use ere_test_utils::{
-    guest::Platform,
-    program::{basic::BasicProgram, Program},
-};
+use ere_platform_sp1::{sp1_zkvm, SP1Platform};
+use ere_test_utils::program::{basic::BasicProgram, Program};
 
 sp1_zkvm::entrypoint!(main);
-
-struct SP1Platform;
-
-impl Platform for SP1Platform {
-    fn read_input() -> Vec<u8> {
-        sp1_zkvm::io::read_vec()
-    }
-
-    fn write_output(output: &[u8]) {
-        sp1_zkvm::io::commit_slice(output);
-    }
-}
 
 pub fn main() {
     BasicProgram::run::<SP1Platform>();
