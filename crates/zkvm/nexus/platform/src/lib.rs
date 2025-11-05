d546b841a23f631935ee7b27ac644d745426d3d3
han0110 <tinghan0110@gmail.com>
2025-11-05 07:57:06 +0000

    feat: update `ere-platform-nexus`

--- crates/zkvm/nexus/platform/src/lib.rs
+++ crates/zkvm/nexus/platform/src/lib.rs
@@ -3,18 +3,24 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
+use core::marker::PhantomData;
+use ere_platform_trait::output_hasher::OutputHasher;
 
-pub use ere_platform_trait::Platform;
+pub use ere_platform_trait::{
+    Platform,
+    output_hasher::{IdentityOutput, PaddedOutput, digest::typenum},
+};
 pub use nexus_rt;
 
-pub struct NexusPlatform;
+pub struct NexusPlatform<H = IdentityOutput>(PhantomData<H>);
 
-impl Platform for NexusPlatform {
+impl<H: OutputHasher> Platform for NexusPlatform<H> {
     fn read_whole_input() -> Vec<u8> {
         nexus_rt::read_private_input().unwrap()
     }
 
     fn write_whole_output(output: &[u8]) {
-        nexus_rt::write_public_output(&output).unwrap()
+        let hash = H::output_hash(output);
+        nexus_rt::write_public_output(&*hash).unwrap()
     }
 }
