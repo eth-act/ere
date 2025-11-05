262e22ba1eb6bae0d638a1803c00563d4d27c8ca
han0110 <tinghan0110@gmail.com>
2025-11-05 07:58:25 +0000

    feat: update `ere-platform-sp1`

--- crates/zkvm/sp1/platform/src/lib.rs
+++ crates/zkvm/sp1/platform/src/lib.rs
@@ -3,18 +3,24 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
+use core::marker::PhantomData;
+use ere_platform_trait::output_hasher::OutputHasher;
 
-pub use ere_platform_trait::Platform;
+pub use ere_platform_trait::{
+    Platform,
+    output_hasher::{IdentityOutput, PaddedOutput, digest::typenum},
+};
 pub use sp1_zkvm;
 
-pub struct SP1Platform;
+pub struct SP1Platform<H = IdentityOutput>(PhantomData<H>);
 
-impl Platform for SP1Platform {
+impl<H: OutputHasher> Platform for SP1Platform<H> {
     fn read_whole_input() -> Vec<u8> {
         sp1_zkvm::io::read_vec()
     }
 
     fn write_whole_output(output: &[u8]) {
-        sp1_zkvm::io::commit_slice(output);
+        let hash = H::output_hash(output);
+        sp1_zkvm::io::commit_slice(&*hash);
     }
 }
