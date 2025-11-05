ef99599ac4e8fc8311e13bed576640fbc6420b6e
han0110 <tinghan0110@gmail.com>
2025-11-05 07:58:03 +0000

    feat: update `ere-platform-pico`

--- crates/zkvm/pico/platform/src/lib.rs
+++ crates/zkvm/pico/platform/src/lib.rs
@@ -3,18 +3,24 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
+use core::marker::PhantomData;
+use ere_platform_trait::output_hasher::OutputHasher;
 
-pub use ere_platform_trait::Platform;
+pub use ere_platform_trait::{
+    Platform,
+    output_hasher::{IdentityOutput, PaddedOutput, digest::typenum},
+};
 pub use pico_sdk;
 
-pub struct PicoPlatform;
+pub struct PicoPlatform<H = IdentityOutput>(PhantomData<H>);
 
-impl Platform for PicoPlatform {
+impl<H: OutputHasher> Platform for PicoPlatform<H> {
     fn read_whole_input() -> Vec<u8> {
         pico_sdk::io::read_vec()
     }
 
     fn write_whole_output(output: &[u8]) {
-        pico_sdk::io::commit_bytes(output);
+        let hash = H::output_hash(output);
+        pico_sdk::io::commit_bytes(&*hash);
     }
 }
