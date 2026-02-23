37e0024565aecb479b3548d942dcb4c3756cdd47
han0110 <tinghan0110@gmail.com>
2026-02-23 06:41:11 +0000

    feat: add `Compiler` impl `RustRv64ima` in `ere-zisk`

---
+++ tests/zisk/stock_nightly_no_std/src/memcpy.s
@@ -0,0 +1,16 @@
+# Copied from https://github.com/0xPolygonHermez/zisk/blob/v0.16.0/ziskos/entrypoint/src/dma/memcpy.s
+
+        .section ".note.GNU-stack","",@progbits
+        .text
+        .attribute      4, 16
+        .attribute      5, "rv64im"
+        .globl  memcpy
+        .p2align        4
+        .type   memcpy,@function
+memcpy:
+        csrs    0x813, a2                  # Marker: Write count (a2) to CSR 0x813
+        add	x0,a0,a1
+        ret
+
+        .size memcpy, .-memcpy
+        .section .text.hot,"ax",@progbits
