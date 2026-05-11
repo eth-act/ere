74214771153b257d3db2309612c051ee8d3faec0
han0110 <tinghan0110@gmail.com>
2026-05-11 09:39:03 +0000

    chore: refactor python script

--- .github/scripts/show-program-vk-and-proof-size.py
+++ .github/scripts/show-program-vk-and-proof-size.py
@@ -4,17 +4,33 @@ import math
 import os
 
 
-def h(n):
-    if n < 1024:
-        return str(n)
-    for u in "KMGT":
-        n /= 1024
-        if n < 1024:
-            return f"{math.ceil(n)}{u}" if n >= 10 else f"{math.ceil(n * 10) / 10:.1f}{u}"
-    return f"{math.ceil(n)}T"
+def format_size(size):
+    if size < 1024:
+        return str(size)
+    for unit in "KM":
+        size /= 1024
+        if size < 1024:
+            return (
+                f"{math.ceil(size)} {unit}iB"
+                if size >= 10
+                else f"{math.ceil(size * 10) / 10:.1f} {unit}iB"
+            )
+    return f"{math.ceil(size)} MiB"
 
 
-D = {
+def print_table(header, body):
+    widths = [
+        max(len(row[col]) for row in [header] + body) for col in range(len(header))
+    ]
+    for row in [header, ("-" * width for width in widths)] + body:
+        print(
+            "| "
+            + " | ".join(cell.ljust(widths[col]) for col, cell in enumerate(row))
+            + " |"
+        )
+
+
+ZKVM = {
     "airbender": "Airbender",
     "openvm": "OpenVM",
     "risc0": "RISC Zero",
@@ -22,19 +38,15 @@ D = {
     "zisk": "ZisK",
 }
 
-s = {
-    (z, k): h(os.path.getsize(f"crates/verifier/{z}/tests/fixtures/{k}.bin"))
-    for z in D
-    for k in ("program_vk", "proof")
+sizes = {
+    (zkvm, obj): format_size(
+        os.path.getsize(f"crates/verifier/{zkvm}/tests/fixtures/{obj}.bin")
+    )
+    for zkvm in ZKVM
+    for obj in ("program_vk", "proof")
 }
 
-rows = [("zkvm", "program vk", "proof")] + [
-    (D[z], s[z, "program_vk"], s[z, "proof"]) for z in D
-]
-w = [max(len(r[i]) for r in rows) for i in range(3)]
-row = lambda r: "| " + " | ".join(c.ljust(w[i]) for i, c in enumerate(r)) + " |"
-
-print(row(rows[0]))
-print("| " + " | ".join("-" * x for x in w) + " |")
-for r in rows[1:]:
-    print(row(r))
+print_table(
+    ("zkVM", "ProgramVK", "Proof"),
+    [(ZKVM[zkvm], sizes[zkvm, "program_vk"], sizes[zkvm, "proof"]) for zkvm in ZKVM],
+)
