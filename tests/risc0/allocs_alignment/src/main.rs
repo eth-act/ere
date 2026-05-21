b1c467e3d4681c6f5994d5526fe471b3960f429c
han0110 <tinghan0110@gmail.com>
2026-05-21 07:20:27 +0000

    fix: risc0 guest

--- tests/risc0/allocs_alignment/src/main.rs
+++ tests/risc0/allocs_alignment/src/main.rs
@@ -1,6 +1,8 @@
 use ere_platform_risc0::risc0_zkvm::guest::env::read_slice;
 
 fn main() {
+    read_slice(&mut [0u8; 4]);
+
     let mut alignment = [0; 4];
     read_slice(&mut alignment);
     let alignment = u32::from_le_bytes(alignment) as usize;
